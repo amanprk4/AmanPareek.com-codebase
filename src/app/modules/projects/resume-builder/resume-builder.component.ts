d181b115c8b067008a8a9f00821af2135d10850c
amanprk4 <amanprk4@gmail.com>
2024-02-16 13:22:26 +0530

    updated styling

---
+++ src/app/modules/projects/resume-builder/resume-builder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-builder',
+  standalone: true,
+  imports: [],
+  templateUrl: './resume-builder.component.html',
+  styleUrl: './resume-builder.component.scss'
+})
+export class ResumeBuilderComponent {
+
+}
