d4f171f0a8fad7c8c50957570db2eeb721a56968
amanprk4 <amanprk4@gmail.com>
2024-04-26 18:18:04 +0530

    Updated resume and text

--- src/app/modules/about-me/about-me.component.ts
+++ src/app/modules/about-me/about-me.component.ts
@@ -14,9 +14,9 @@ export class AboutMeComponent {
   ];
 
   aboutMeBio:any = {
-    short: `<em>Aman Pareek, '96.</em> Front-end wordsmith and Marvel/DC enthusiast. VIT Vellore alum. Passionate about code, movies, and literary adventures. Always ready for a road trip or a scenic car drive. <em>Let's craft digital tales!</em> 🚀📚 #TechWordsmith`,
+    short: `<em>Aman Pareek, born '96.</em> Front-end wordsmith and Marvel/DC enthusiast. VIT Vellore alum. Passionate about code, movies, and literary adventures. Always ready for a road trip or a scenic car drive. <em>Let's craft digital tales!</em> 🚀📚 #TechWordsmith`,
     medium: `<em>Aman Pareek, born '96,</em> crafts digital narratives as a front-end scribe. Marvel/DC savant, VIT Vellore alumnus <em>dissecting code during the day and deciphering cosmic stories by night.</em> Passionate about code, movies, and literary adventures. Always up for a road trip or a scenic car drive. <em>Join me in scripting the next chapter of innovation!</em> 🚀📚✨ #TechScribe #CodeCraftsman`,
-    long: `In the labyrinth of code and creativity,<em> meet Aman Pareek, a '96 VIT Vellore alumnus,</em> scripting digital fables as a front-end storyteller. Born to the skies, <em> an Air Force legacy,</em> I navigate the code by day and interpret cosmic tales by night.
+    long: `In the labyrinth of code and creativity,<em> meet Aman Pareek, born '96, a 2019 VIT Vellore alumnus,</em> scripting digital fables as a front-end storyteller. Born to the skies, <em> an Air Force legacy,</em> I navigate the code by day and interpret cosmic tales by night.
 
     A Marvel/DC connoisseur, <em>each line of code contributes to the narrative.</em> As a cinema savant and bookish bard, inspiration dances between screens and pages. Passionate about code, movies, and literary adventures. Always up for a road trip or a scenic car drive, <em>join me in weaving the fabric of innovation!</em> 🚀📚✨ #TechStoryteller #DigitalWordsmith`,
   };
